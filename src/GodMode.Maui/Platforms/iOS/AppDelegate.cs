f33ff60adb92ad7817f16290d515d0123f63ff1f
jjj <john.juul.jensen@hotmail.com>
2026-02-28 01:14:36 +0100

    Removing MAUI

--- src/GodMode.Maui/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace GodMode.Maui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
