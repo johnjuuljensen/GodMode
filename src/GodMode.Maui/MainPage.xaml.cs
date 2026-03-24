40972e95f642d208426ec8708c1f20403eb1cdf8
jjj <john.juul.jensen@hotmail.com>
2026-03-24 18:04:41 +0100

    maui fixes

--- src/GodMode.Maui/MainPage.xaml.cs
+++ src/GodMode.Maui/MainPage.xaml.cs
@@ -10,6 +10,19 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         _bridge = new HostBridge(WebView);
+
+#if WINDOWS
+        WebView.HandlerChanged += (s, e) =>
+        {
+            if (WebView.Handler?.PlatformView is Microsoft.UI.Xaml.Controls.WebView2 wv2)
+            {
+                wv2.CoreWebView2Initialized += (_, _) =>
+                {
+                    wv2.CoreWebView2.Settings.AreDevToolsEnabled = true;
+                };
+            }
+        };
+#endif
     }
 
     private void WebView_RawMessageReceived(object? sender, HybridWebViewRawMessageReceivedEventArgs e)
--- src/GodMode.Maui/MauiProgram.cs
+++ src/GodMode.Maui/MauiProgram.cs
@@ -4,6 +4,16 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
+#if WINDOWS
+        // WebView2 serves our own React app from https://0.0.0.1 which is cross-origin
+        // to any GodMode.Server. Disable web security so SignalR can connect freely.
+        var args = "--disable-web-security";
+#if DEBUG
+        args += " --auto-open-devtools-for-tabs";
+#endif
+        Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", args);
+#endif
+
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
